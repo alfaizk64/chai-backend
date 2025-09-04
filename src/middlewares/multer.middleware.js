4127a5a99169b2bd062f50a2ee91c1ae05b47f3a
alfaiz khan <84037292+alfaizk64@users.noreply.github.com>
2025-09-04 17:22:23 +0530

    multer and cloudinary setup

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,16 @@
+import multer from 'multer'
+import { v4 as uuidv4 } from "uuid"; // generate unique IDs
+import path from "path"
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "/Public/temp")
+  },
+  filename: function (req, file, cb) {
+     const uniqueName = `${uuidv4()}${path.extname(file.originalname)}`;
+    cb(null, uniqueName)
+  }
+})
+
+export const upload = multer({ storage })
+
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_API_SECRET_KEY,
+});
+
+export const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    // upload on cloudinary
+    const uploadResponse = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded sucessfully
+    console.log("File is uploaded on cloudinary", uploadResponse?.url);
+
+    return uploadResponse;
+  } catch (error) {
+    fs.unlink(localFilePath)
+    // removed local savewtemp file 
+  }
+};
